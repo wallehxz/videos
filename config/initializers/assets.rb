fa8873efee10486ecd218aae8e3cfbe5bc79efa4
Huaxuan <huaxuan21475@gmail.com>
2015-12-11 11:58:57 +0800

    视频播放器本地引用

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += [/.*\.js*/, /.*\.css*/]
-Rails.application.config.assets.precompile += %w( LTE_admin/lte_admin.css )
+Rails.application.config.assets.precompile += %w( LTE_admin/lte_admin.css)
+Rails.application.config.assets.precompile += %w( video_player-4.2.js  video_player.css )
